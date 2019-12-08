9641389d885443682fe76b299a6099cf83249c84
sriramrudraraju <sriram.rudraraju6@gmail.com>
2019-12-08 11:37:25 -0600

    Updates

---
+++ precache-manifest.837abea571aad7fe43545167aa3b495c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e0831f1813c8b5ccbd5f8769ab81749",
+    "url": "/fed-take-home/index.html"
+  },
+  {
+    "revision": "eff4822c6500bef2fd04",
+    "url": "/fed-take-home/static/css/main.a83e5236.chunk.css"
+  },
+  {
+    "revision": "dcb688ed889cd70f52d0",
+    "url": "/fed-take-home/static/js/2.4e9f3db3.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/fed-take-home/static/js/2.4e9f3db3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eff4822c6500bef2fd04",
+    "url": "/fed-take-home/static/js/main.04f10799.chunk.js"
+  },
+  {
+    "revision": "061288c36bb119d123b8",
+    "url": "/fed-take-home/static/js/runtime-main.6c564632.js"
+  },
+  {
+    "revision": "bd72fcdd59d397350df05056a5bafd10",
+    "url": "/fed-take-home/static/media/stanger-things_raw.bd72fcdd.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sriramrudraraju/fed-take-home/precache-manifest.ee7feeb7191dc3e0b625456284ae282d.js"
+  "/fed-take-home/precache-manifest.837abea571aad7fe43545167aa3b495c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sriramrudraraju/fed-take-home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fed-take-home/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
